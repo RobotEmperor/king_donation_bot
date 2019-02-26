400801a9604b235cb20c870b29447f75166851b3
robotemperor <kimpd1224@gmail.com>
2019-02-26 23:28:27 +0900

    m

--- mobile_manager/src/mobile_manager_node.cpp
+++ mobile_manager/src/mobile_manager_node.cpp
@@ -139,14 +139,14 @@ void arrivals_action_command_callback(const std_msgs::Int8::ConstPtr& msg)
   break;
   case 4:
   {
-    max_speed = 25;
+    max_speed = 21;
     rotation_left = 1;
     rotation_right = 0;
   }
   break;
   case 5:
   {
-    max_speed = 50;
+    max_speed = 21;
     rotation_left = 0;
     rotation_right = 1;
   }
